404f48e0028e37c2dee0beb7127b42b52603916e
Alice Morin <alicemorin@gmail.com>
2015-08-18 18:42:49 +0200

    create wiki article controller model some views missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |article|
+  article = Article.create(title: Faker::Name.title, content: Faker::Lorem.paragraph)
+  article.save
+end
